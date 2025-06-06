ec21b944d2befc33c8c17aae32dcd13942bfdffe
Vinicius Gabriel <vini.gabriell@outlook.com>
2025-06-06 14:21:41 -0300

    fix: App.tsx

--- App.tsx
+++ App.tsx
@@ -1,8 +1,23 @@
+import {GluestackUIProvider} from '@gluestack-ui/themed';
 import React from 'react';
+import {StatusBar} from 'react-native';
 import {AppNavigator} from './src/routes/AppNavigator';
+import {useThemeStore} from './src/store/useThemeStore';
+import {useAppTheme} from './src/theme';
 
 const App = () => {
-  return <AppNavigator />;
+  const {theme} = useAppTheme();
+  const {isDark} = useThemeStore();
+
+  return (
+    <GluestackUIProvider config={theme}>
+      <StatusBar
+        barStyle={isDark ? 'light-content' : 'dark-content'}
+        backgroundColor={theme.colors.background}
+      />
+      <AppNavigator />
+    </GluestackUIProvider>
+  );
 };
 
 export default App;
